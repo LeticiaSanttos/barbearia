ce5efc05f6cea4cc209dae5b3cbe384a1b58441a
Letícia Santtos C <santoscastroleticia@gmail.com>
2024-08-10 11:12:24 -0300

    add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,33 @@
+import { SearchIcon } from "lucide-react"
 import { Button } from "./_components/ui/button"
+import Header from "./_components/ui/header"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="front-bold text-xl">Olá, Letícia!</h2>
+        <p>Sábado, 10 de agosto.</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barbearia"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
 
-  return <Button>text</Button>
-};
-
-export default Home;
+export default Home
